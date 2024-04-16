fb05104be9d04da65e3cb710128e75e7a9adf8c5
Tomi Rämö <47462643+TomiRam@users.noreply.github.com>
2024-04-16 07:33:45 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,50 @@
+
+import './App.css';
+import Meikä from './assets/Meikä.png';
+import React,{ useState, useEffect } from "react";
+
+  
+
+function App() {
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const formattedTime = currentTime.toLocaleTimeString('en-US', {
+    hour12: false, 
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit'
+  });
+  
+  return (
+    <div className="App">
+   
+        <header className="App-header">
+        <img src={Meikä} alt="Meikä" />
+        <h1>Tomi Rämö</h1>
+        <p>
+        Hello Im a indie game developer from Finland and 
+        im currently developing my first indie game with Unity . 
+        </p>
+        <a href="https://github.com/TomiRam" >Github</a>
+        <a href="https://www.linkedin.com/in/tomi-r%C3%A4m%C3%B6-54b925153/" >LinkedIn</a>
+        <a href ="https://tomiram.itch.io/">Itchio</a>
+        <a href ="https://www.instagram.com/tomstrongsteel/">Instagram</a>
+        <a href ="https://www.facebook.com/tomi.a.ramo">Facebook</a>
+        <a href="mailto:t0m1r4m@gmail.com"> Send an Email to me</a>
+        <h2>Current Time:{formattedTime}</h2>
+      </header>
+     
+    </div>
+    
+  );
+}
+
+export default App;
